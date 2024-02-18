d73fb2f0cfcb0a30b6806edac6aeb855e4b7e764
archspire-9k <107560424+archspire-9k@users.noreply.github.com>
2024-02-18 11:42:52 -0500

    project setup

---
+++ interpreter.rkt
@@ -0,0 +1,42 @@
+#lang racket
+(require "simpleParser.rkt")
+
+; Returns a integer value
+(define Minteger
+  (lambda (expr state)
+    (cond
+    ((number? expr)                        expr)
+    ((var? expr)                           (M_var_value expr state))
+    ((isNeg? expr)                         (-         0                                       (Minteger(leftoperand  expr) state)))
+    ((subtract? expr)                      (-         (Minteger(leftoperand expr) state)      (Minteger(rightoperand expr) state)))
+    ((add? expr)                           (+         (Minteger(leftoperand expr) state)      (Minteger(rightoperand expr) state)))
+    ((multiply? expr)                      (*         (Minteger(leftoperand expr) state)      (Minteger(rightoperand expr) state)))
+    ((divide? expr)                        (quotient  (Minteger(leftoperand expr) state)      (Minteger(rightoperand expr) state)))
+    ((remainder? expr)                     (remainder (Minteger(leftoperand expr) state)      (Minteger(rightoperand expr) state)))
+    (else                                  "No valid operator"))))
+
+; Returns a value using a handler based value type
+(define M_value
+  (lambda (lis state)
+    (cond
+      ((number? lis)                        lis)
+      ((var? lis)              (M_var_value lis state))
+      ((boolean? lis)                       lis)
+      ((isArithmetic? lis)     (Minteger    lis state))
+      ((isBoolean? lis state)  (M_boolean   lis state))
+      (else                    "cannot evaluate"
+      ))))
+
+; Categorizes a boolean statement and calls a respective handler if neccessary
+; If the statement is an explicit boolean return #t/#f respectively
+(define M_boolean
+  (lambda (lis state)
+    (cond
+      ((number?             lis)     (error "Number not a boolean"))
+      ((isTrue?             lis)     #t)
+      ((isFalse?            lis)     #f)
+      ((boolean?            lis)     lis)
+      ((var?                lis)     (M_var_value lis state))
+      ((isComparison?       lis)     (M_boolean_comparison lis state))
+      ((isBooleanOperation? lis)     (M_boolean_op lis state))
+      (else                          (error "Invalid Logic Operand(s)")))))
